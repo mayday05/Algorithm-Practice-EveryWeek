b6c63a34f322d1a3a661b4b924b18d5fce3c4b5a
mayday05 <956899815@qq.com>
2019-07-06 15:27:49 +0800

    // 7月更新算法练习

---
+++ src/main/java/cn/mayday/algorithmpractice/AlgorithmPracticeApplication.java
@@ -0,0 +1,13 @@
+package cn.mayday.algorithmpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmPracticeApplication.class, args);
+    }
+
+}
